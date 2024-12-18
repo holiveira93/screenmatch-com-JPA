26c52c9f29badb16958d1cc8ce376d190f063260
holiveira93 <holiveira_93@hotmail.com>
2024-12-18 17:48:10 -0300

    commit

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,17 +5,17 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
 
-    public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("");
-
-        CompletionRequest requisicao = CompletionRequest.builder()
-                .model("gpt-3.5-turbo-instruct")
-                .prompt("traduza para o português o texto: " + texto)
-                .maxTokens(1000)
-                .temperature(0.7)
-                .build();
-
-        var resposta = service.createCompletion(requisicao);
-        return resposta.getChoices().get(0).getText();
-    }
+//    public static String obterTraducao(String texto) {
+//        OpenAiService service  new OpenAiService("");
+//
+//        CompletionRequest requisicao = CompletionRequest.builder()
+//                .model("gpt-3.5-turbo-instruct")
+//                .prompt("traduza para o português o texto: " + texto)
+//                .maxTokens(1000)
+//                .temperature(0.7)
+//                .build();
+//
+//        var resposta = service.createCompletion(requisicao);
+//        return resposta.getChoices().get(0).getText();
+//    }
 }
