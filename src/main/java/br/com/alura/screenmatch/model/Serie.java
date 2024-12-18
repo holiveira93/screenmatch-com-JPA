48996068d95108a094b704ebed79da9014e3d71f
holiveira93 <holiveira_93@hotmail.com>
2024-12-18 17:31:44 -0300

    aula 1.4 criada a classe ConsultaChatGPT e refatorado  a classe SÃerie para realizar a tradução da sinopse

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,5 +1,7 @@
 package br.com.alura.screenmatch.model;
 
+import br.com.alura.screenmatch.service.ConsultaChatGPT;
+
 import java.util.Optional;
 import java.util.OptionalDouble;
 
@@ -19,7 +21,9 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = dadosSerie.sinopse();
+//        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+        this.sinopse = dadosSerie.sinopse().trim();
+
     }
 
     public String getTitulo() {
