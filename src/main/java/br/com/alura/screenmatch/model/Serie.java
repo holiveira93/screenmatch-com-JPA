9e67a781c6cb029112d9a5a47ac6df039c89d209
holiveira93 <holiveira_93@hotmail.com>
2024-12-18 17:42:35 -0300

    refatorado para utilizar 'MyMemory' para traducao de sinopse como opçcao gratuita ao 'ChatGPT'

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,8 +1,7 @@
 package br.com.alura.screenmatch.model;
 
-import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import br.com.alura.screenmatch.service.tradu.ConsultaMyMemory;
 
-import java.util.Optional;
 import java.util.OptionalDouble;
 
 public class Serie {
@@ -22,7 +21,7 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
 //        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
-        this.sinopse = dadosSerie.sinopse().trim();
+        this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
 
     }
 
