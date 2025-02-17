5a2abba7c07c1e901f2239da00a2f614f7bc09f4
holiveira93 <holiveira_93@hotmail.com>
2025-02-17 13:39:52 -0300

    aula 2.2 Representando séries do banco

---
+++ src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.dto;
+
+import br.com.alura.screenmatch.model.Categoria;
+
+public record SerieDTO(Long Id,
+                       String titulo,
+                       Integer totalTemporadasDouble,
+                       Double avaliacao,
+                       Categoria genero,
+                       String atores,
+                       String poster,
+                       String sinopse) {
+}
